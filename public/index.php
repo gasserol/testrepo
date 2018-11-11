28dd28171279456e3c8534c03473e5298add7407
gasserol <oliver.gasser@intervisio.ch>
2018-11-11 13:01:45 +0100

    added new text to git

--- public/index.php
+++ public/index.php
@@ -1,4 +1,4 @@
 <?php 
-	echo 'Welcome to runcloud, und hallo Tatjana! Han di lieb ganz fescht!!!!!';
+	echo 'Welcome to runcloud, und hallo Tatjana! Han di lieb ganz fescht gäng no!!!!!';
 	//phpinfo();
 ?>
